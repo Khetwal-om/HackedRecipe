ab4b1c2d4ddb681ee7952b17bdc179f1ceb9a07a
khetwal-om <khetwalom@gmail.com>
2020-08-09 18:45:29 +0530

    Updates

---
+++ precache-manifest.d222cfe4121a4e9f8b437b3459e24bf6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51b0f3362cbf60aecc899fd11d14b7f2",
+    "url": "/HackedRecipe/index.html"
+  },
+  {
+    "revision": "13b6d0f7cb75e9933201",
+    "url": "/HackedRecipe/static/css/main.3a1f56c6.chunk.css"
+  },
+  {
+    "revision": "4429680f6418a6e216f9",
+    "url": "/HackedRecipe/static/js/2.7915f5ae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/HackedRecipe/static/js/2.7915f5ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13b6d0f7cb75e9933201",
+    "url": "/HackedRecipe/static/js/main.5027a657.chunk.js"
+  },
+  {
+    "revision": "cfcb477b2bfcf2ba42ce",
+    "url": "/HackedRecipe/static/js/runtime-main.db8bfe1a.js"
+  },
+  {
+    "revision": "7cd92069f47b48e50dedd69f7a704558",
+    "url": "/HackedRecipe/static/media/Illustration1.7cd92069.png"
+  },
+  {
+    "revision": "dfb2ea3c2ab3a8dafafe2226ac6fb008",
+    "url": "/HackedRecipe/static/media/Illustration2.dfb2ea3c.png"
+  },
+  {
+    "revision": "fd3a054f1abfa8be8cc81359718f3ca6",
+    "url": "/HackedRecipe/static/media/Illustration3.fd3a054f.png"
+  },
+  {
+    "revision": "df92c81182058492697117d089016820",
+    "url": "/HackedRecipe/static/media/Illustration4.df92c811.png"
+  }
+]);
